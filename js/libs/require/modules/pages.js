c2ef70e0924028450ea9395a1873959931794e29
AkkiParekh <ankit77parekh@gmail.com>
2016-08-07 11:25:39 -0700

    optimizing

--- js/libs/require/modules/pages.js
+++ js/libs/require/modules/pages.js
@@ -1,23 +1,27 @@
 requirejs(['./common'], function () {
 
-	var page =  (typeof this.page != "undefined") ? this.page : "home";
-
-	switch(page) {
-		case "home":
-			requirejs(['jquery', 'materialize', 'lazy', 'data', 'owlCarousel', 'mobileDetect', 'site'], function () {		
-				console.log("jquery and dependencies configured...");
-				virtual.initialize();
-			});				
-			break;
-		case "culture":
-		case "approach":
-		case "cases":
-			requirejs(['jquery', 'materialize', 'lazy', 'data', 'site'], function () {		
-				console.log("jquery and dependencies configured...");
-				virtual.initialize();
-			});
-			break;
-		default:
-			break;
-	}
+	var page = "";
+	requirejs(['jquery'], function () {		
+		console.log("jquery configured...");
+		page =  (typeof $("body").data("page") != "undefined") ? $("body").data("page") : "home";
+	
+		switch(page) {
+			case "home":
+				requirejs(['materialize', 'lazy', 'data', 'owlCarousel', 'mobileDetect', 'site'], function () {		
+					console.log("jquery and dependencies configured...");
+					virtual.initialize();
+				});				
+				break;
+			case "culture":
+			case "approach":
+			case "cases":
+				requirejs(['materialize', 'lazy', 'data', 'site'], function () {		
+					console.log("jquery and dependencies configured...");
+					virtual.initialize();
+				});
+				break;
+			default:
+				break;
+		}
+	});		
 });
