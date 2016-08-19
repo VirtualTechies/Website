eb1782c33b4efb8ffd04ff6d6fa15c1d569d2665
AkkiParekh <ankit77parekh@gmail.com>
2016-08-19 10:00:24 -0700

    sncincg

--- js/virtual.js
+++ js/virtual.js
@@ -94,10 +94,10 @@ virtual = {
 		footer += '<div class="container">';
 		footer += '<div class="row">';
 		footer += '<div class="col s12 m6 l6 center-align">';
-		footer += '<p>© '+new Date().getFullYear()+' Virtual Techies. All Rights Reserved.</p>';
+		footer += '<p class="no-mar">© '+new Date().getFullYear()+' Virtual Techies. All Rights Reserved.</p>';
 		footer += '</div>';
 		footer += '<div class="col s12 m6 l6 center-align">';
-		footer += '<p>Designed & Developed by <a href="http://akkiparekhcv.appspot.com/">Ankit Parekh</a></p>';
+		footer += '<p class="no-mar">Designed & Developed by <a href="http://akkiparekhcv.appspot.com/">Ankit Parekh</a></p>';
 		footer += '</div></div></div></div>';
 
 		if($('footer').length){
